2ffea24e256c1a8e57864054af5ba349040d9c97
Mauricio Fow <mauriciofow@gmail.com>
2019-09-21 19:45:30 -0500

    adas

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.use(
 		graphiql  : isDev
 	})
 );
-app.listen(port, () => {
-	console.log('server on ps');
+app.listen(process.env.PORT || 4000, function() {
+	console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
